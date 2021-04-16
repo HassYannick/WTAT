6ac4d712ea87ba817a4f16243665b484db54959a
HassYannick <56965354+HassYannick@users.noreply.github.com>
2021-04-16 19:01:23 +0200

    main.js udate

--- L4_simple_web_server/main.js
+++ L4_simple_web_server/main.js
@@ -5,11 +5,12 @@ const port = 3000;
         console.log("Received an incoming request!");
         response.writeHead(httpStatus.OK, {
             "Content-Type": "text/html"
-            });
-            let responseMessage = "<h1>Hello, Universe!</h1>";
-            response.write(responseMessage);
-            response.end();
-            console.log(`Sent a response : ${responseMessage}`);
-    });    
+        });
+        let responseMessage = "<h1>Hello, Universe!</h1>";
+        response.write(responseMessage);
+        response.end();
+        console.log(`Sent a response : ${responseMessage}`);
+    }); 
+
 app.listen(port);
 console.log(`The server has started and is listening on port number: ${port}`);
